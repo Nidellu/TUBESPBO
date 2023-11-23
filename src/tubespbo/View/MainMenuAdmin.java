d66806d0e92a0a0a91aa77911edca651dc21b1d8
ollynth <carollynthiow@gmail.com>
2023-11-23 13:18:21 +0700

    create admin button for checking income

--- src/tubespbo/View/MainMenuAdmin.java
+++ src/tubespbo/View/MainMenuAdmin.java
@@ -82,25 +82,36 @@ public class MainMenuAdmin {
             }
         });
 
-        JTable table = new JTable(model) {
-            public Class getColumnClass(int column) {
-                return (column == 0) ? Icon.class : Object.class;
+        // JTable table = new JTable(model) {
+        //     public Class getColumnClass(int column) {
+        //         return (column == 0) ? Icon.class : Object.class;
+        //     }
+        // };
+        // table.setPreferredScrollableViewportSize(table.getPreferredSize());
+
+        // table.setBounds(50, 100, 700, 200);
+        // JScrollPane sp = new JScrollPane(table);
+        // table.setRowHeight(50);
+
+    // button buat check income
+        JButton checkIncome = new JButton("Check Income");
+        checkIncome.setBounds(560, 150, 200, 30);
+        checkIncome.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                f.dispose();
+                new CheckIncomeAdmin(id);
             }
-        };
-        table.setPreferredScrollableViewportSize(table.getPreferredSize());
+        });
 
-        table.setBounds(50, 100, 700, 200);
-        JScrollPane sp = new JScrollPane(table);
-        table.setRowHeight(50);
-
+        // adding element into frame
         JPanel panel = new JPanel(null);
         f.setLayout(null);
         f.setSize(500, 600);
-        f.add(backButton);
-        f.add(addPromoBtn);
-        f.add(seePromoBtn);
-        f.add(table);
-        f.add(sp);
+        f.add(backButton);  // add back button
+        f.add(addPromoBtn); // add promo button
+        f.add(seePromoBtn); // add see promo button
+        // f.add(table);
+        // f.add(sp);
         f.setVisible(true);
     }
 
