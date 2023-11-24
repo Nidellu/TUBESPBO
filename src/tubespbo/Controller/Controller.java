454631900df8829c3342788669c6697860cb2304
ollynth <carollynthiow@gmail.com>
2023-11-24 17:40:22 +0700

    fix on off button

--- src/tubespbo/Controller/Controller.java
+++ src/tubespbo/Controller/Controller.java
@@ -842,7 +842,7 @@ public class Controller {
 // order ride end
 
 // start switch on off status driver
-    private String getDriverStat(int driverID) {
+    public String getDriverStat(int driverID) {
         String stat = "";
         conn.connect();
         String statsQuery = "SELECT driver_status FROM drivers WHERE driver_id = '" + driverID + "';";
@@ -866,7 +866,7 @@ public class Controller {
         } else if ("AVAILABLE".equalsIgnoreCase(currentStatus)) {
             return "Now Available";
         } else {
-            return "Unknown Status";
+            return "In Order";
         }
     }
 
